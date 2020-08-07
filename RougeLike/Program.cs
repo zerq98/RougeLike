397cde0728d10e27640c4e87d8c8ff3b2a41cc55
Mateusz Trybuła <zerq1998@gmail.com>
2020-08-07 23:56:55 +0200

    Add project files.

---
+++ RougeLike/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace RougeLike
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome in the GAME Adventurer!");
+            Console.WriteLine("                               ");
+            Console.WriteLine("1. New game");
+            Console.WriteLine("2. Load Game");
+            Console.WriteLine("3. Remove data");
+            Console.WriteLine("4. Credits");
+            Console.WriteLine("5. Exit");
+        }
+    }
+}
